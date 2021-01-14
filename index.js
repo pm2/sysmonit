157312261b16547c254a9c938f11d63403aae7b4
Unitech <alexandre@keymetrics.io>
2021-01-14 11:43:39 +0100

    1.1.0 - monitor pm2 and agent - reformat iface/fs metrics name

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 
 const rx2 = require('rx2')
 const SystemInfos = require('./src/SystemInfos.js')
+const PM2Infos = require('./src/pm2.js')
 
 class SysMonit {
   constructor() {
     this.sysinfos = new SystemInfos()
     this.report = {}
     this.pass = 0
+
+    this.pm2infos = new PM2Infos()
+    this.pm2_report = {}
+  }
+
+  start() {
+    this.sysinfos.startCollection()
+    this.report = this.sysinfos.report()
+
+    this.pm2infos.startCollection()
+    this.pm2_report = this.pm2infos.report()
+
+    this.bindActions()
+
+    setInterval(() => {
+      if (this.pass++ < 4)
+        this.bindMetrics()
+      this.report = this.sysinfos.report()
+      this.pm2_report = this.pm2infos.report()
+
+      if (process.env.VERBOSE) {
+        console.log(JSON.stringify(this.report, '', 2))
+        console.log(JSON.stringify(this.pm2_report, '', 2))
+      }
+    }, 1000)
   }
 
   bindActions() {
@@ -16,6 +42,15 @@ class SysMonit {
   }
 
   bindMetrics() {
+    rx2.metric('pm2:cpu', () => this.pm2_report.pm2.cpu)
+    rx2.metric('pm2:mem', () => this.pm2_report.pm2.mem)
+
+    rx2.metric('pm2:agent:cpu', () => this.pm2_report.agent.cpu)
+    rx2.metric('pm2:agent:mem', () => this.pm2_report.agent.mem)
+
+    /**
+     * From Sysinfo
+     */
     rx2.metric('cpu', () => this.report.cpu.load)
     rx2.metric('cpus', () => this.report.cpu.loads)
     rx2.metric('cpu:temp', () => this.report.cpu.temperature)
@@ -29,19 +64,17 @@ class SysMonit {
 
     this.report.storage.filesystems.forEach((fss, i) => {
       if (!fss.fs) return
-      rx2.metric(`fs:${fss.mount}:use`, () => this.report.storage.filesystems[i].use)
-      rx2.metric(`fs:${fss.mount}:size`, () => Math.floor(this.report.storage.filesystems[i].size / 1024 / 1024))
+      rx2.metric(`fs:use:${fss.mount}`, () => this.report.storage.filesystems[i].use)
+      rx2.metric(`fs:size:${fss.mount}`, () => Math.floor(this.report.storage.filesystems[i].size / 1024 / 1024))
     })
 
-    //rx2.metric(`net:default`, () => this.report.default_interface)
-
     Object.keys(this.report.network).forEach(iface => {
-      rx2.metric(`net:${iface}:tx_5`, () => this.report.network[iface].tx_5)
-      rx2.metric(`net:${iface}:rx_5`, () => this.report.network[iface].rx_5)
-      rx2.metric(`net:${iface}:rx_errors_60`, () => this.report.network[iface].rx_errors_60)
-      rx2.metric(`net:${iface}:tx_errors_60`, () => this.report.network[iface].tx_errors_60)
-      rx2.metric(`net:${iface}:rx_dropped_60`, () => this.report.network[iface].rx_dropped_60)
-      rx2.metric(`net:${iface}:tx_dropped_60`, () => this.report.network[iface].tx_dropped_60)
+      rx2.metric(`net:tx_5:${iface}`, () => this.report.network[iface].tx_5)
+      rx2.metric(`net:rx_5:${iface}`, () => this.report.network[iface].rx_5)
+      rx2.metric(`net:rx_errors_60:${iface}`, () => this.report.network[iface].rx_errors_60)
+      rx2.metric(`net:tx_errors_60:${iface}`, () => this.report.network[iface].tx_errors_60)
+      rx2.metric(`net:rx_dropped_60:${iface}`, () => this.report.network[iface].rx_dropped_60)
+      rx2.metric(`net:tx_dropped_60:${iface}`, () => this.report.network[iface].tx_dropped_60)
     })
 
     if (this.report.graphics.memTotal) {
@@ -51,20 +84,6 @@ class SysMonit {
     }
 
     //rx2.transpose('report', () => this.report)
-  }
-
-  start() {
-    this.sysinfos.startCollection()
-
-    this.report = this.sysinfos.report()
-
-    this.bindActions()
-
-    setInterval(() => {
-      if (this.pass++ < 4)
-        this.bindMetrics()
-      this.report = this.sysinfos.report()
-    }, 1000)
   }
 }
 
