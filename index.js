204ce0aa215ca36fdd0a394c8d973df0a13ae381
Unitech <alexandre@keymetrics.io>
2021-01-18 22:30:03 +0100

    rx2->tx2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const rx2 = require('rx2')
+const tx2 = require('tx2')
 const SystemInfos = require('./src/SystemInfos.js')
 const PM2Infos = require('./src/pm2.js')
 
@@ -36,54 +36,54 @@ class SysMonit {
   }
 
   bindActions() {
-    rx2.action('info', (cb) => {
+    tx2.action('info', (cb) => {
       cb(this.sysinfos.report())
     })
   }
 
   bindMetrics() {
-    rx2.metric('pm2:cpu', () => this.pm2_report.pm2.cpu)
-    rx2.metric('pm2:mem', () => this.pm2_report.pm2.mem)
+    tx2.metric('pm2:cpu', () => this.pm2_report.pm2.cpu)
+    tx2.metric('pm2:mem', () => this.pm2_report.pm2.mem)
 
-    rx2.metric('pm2:agent:cpu', () => this.pm2_report.agent.cpu)
-    rx2.metric('pm2:agent:mem', () => this.pm2_report.agent.mem)
+    tx2.metric('pm2:agent:cpu', () => this.pm2_report.agent.cpu)
+    tx2.metric('pm2:agent:mem', () => this.pm2_report.agent.mem)
 
     /**
      * From Sysinfo
      */
-    rx2.metric('cpu', () => this.report.cpu.load)
-    rx2.metric('cpus', () => this.report.cpu.loads)
-    rx2.metric('cpu:temp', () => this.report.cpu.temperature)
-    rx2.metric('mem:total', () => this.report.mem.total)
-    rx2.metric('mem:free', () => this.report.mem.free)
-    rx2.metric('mem:active', () => this.report.mem.active)
-    rx2.metric('mem:available', () => this.report.mem.available)
-    rx2.metric('fd', () => this.report.fd.opened)
-    rx2.metric('io:r', () => this.report.storage.io.read)
-    rx2.metric('io:w', () => this.report.storage.io.write)
+    tx2.metric('cpu', () => this.report.cpu.load)
+    tx2.metric('cpus', () => this.report.cpu.loads)
+    tx2.metric('cpu:temp', () => this.report.cpu.temperature)
+    tx2.metric('mem:total', () => this.report.mem.total)
+    tx2.metric('mem:free', () => this.report.mem.free)
+    tx2.metric('mem:active', () => this.report.mem.active)
+    tx2.metric('mem:available', () => this.report.mem.available)
+    tx2.metric('fd', () => this.report.fd.opened)
+    tx2.metric('io:r', () => this.report.storage.io.read)
+    tx2.metric('io:w', () => this.report.storage.io.write)
 
     this.report.storage.filesystems.forEach((fss, i) => {
       if (!fss.fs) return
-      rx2.metric(`fs:use:${fss.mount}`, () => this.report.storage.filesystems[i].use)
-      rx2.metric(`fs:size:${fss.mount}`, () => Math.floor(this.report.storage.filesystems[i].size / 1024 / 1024))
+      tx2.metric(`fs:use:${fss.mount}`, () => this.report.storage.filesystems[i].use)
+      tx2.metric(`fs:size:${fss.mount}`, () => Math.floor(this.report.storage.filesystems[i].size / 1024 / 1024))
     })
 
     Object.keys(this.report.network).forEach(iface => {
-      rx2.metric(`net:tx_5:${iface}`, () => this.report.network[iface].tx_5)
-      rx2.metric(`net:rx_5:${iface}`, () => this.report.network[iface].rx_5)
-      rx2.metric(`net:rx_errors_60:${iface}`, () => this.report.network[iface].rx_errors_60)
-      rx2.metric(`net:tx_errors_60:${iface}`, () => this.report.network[iface].tx_errors_60)
-      rx2.metric(`net:rx_dropped_60:${iface}`, () => this.report.network[iface].rx_dropped_60)
-      rx2.metric(`net:tx_dropped_60:${iface}`, () => this.report.network[iface].tx_dropped_60)
+      tx2.metric(`net:tx_5:${iface}`, () => this.report.network[iface].tx_5)
+      tx2.metric(`net:rx_5:${iface}`, () => this.report.network[iface].rx_5)
+      tx2.metric(`net:rx_errors_60:${iface}`, () => this.report.network[iface].rx_errors_60)
+      tx2.metric(`net:tx_errors_60:${iface}`, () => this.report.network[iface].tx_errors_60)
+      tx2.metric(`net:rx_dropped_60:${iface}`, () => this.report.network[iface].rx_dropped_60)
+      tx2.metric(`net:tx_dropped_60:${iface}`, () => this.report.network[iface].tx_dropped_60)
     })
 
     if (this.report.graphics.memTotal) {
-      rx2.metric('graphics:mem:total', () => this.report.graphics.memTotal)
-      rx2.metric('graphics:mem:used', () => this.report.graphics.memUsed)
-      rx2.metric('graphics:temp', () => this.report.graphics.temperature)
+      tx2.metric('graphics:mem:total', () => this.report.graphics.memTotal)
+      tx2.metric('graphics:mem:used', () => this.report.graphics.memUsed)
+      tx2.metric('graphics:temp', () => this.report.graphics.temperature)
     }
 
-    //rx2.transpose('report', () => this.report)
+    //tx2.transpose('report', () => this.report)
   }
 }
 
